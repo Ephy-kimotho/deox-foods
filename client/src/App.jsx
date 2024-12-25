090d88e354ecfd313b32f9768af6d306fbf9e55f
victorkimathi <victorcodes9532@gmail.com>
2024-12-25 12:37:37 +0300

    Chatbot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import ChatGPTInfo from "./pages/chatbot/chat/page";
+import ChatbotLayout from "./pages/chatbot/layout/page";
 import Contact from "./pages/contact/page";
 function App() {
   return (
     // <h1 className="text-3xl font-bold underline text-red-500">Hello world</h1>
-    <Contact />
+    // <Contact />
+    // <ChatGPTInfo />
+    <ChatbotLayout/>
 
   );
 }
