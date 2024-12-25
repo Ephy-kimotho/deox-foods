8119d3574fdd9ac0828e4e5c2ad695fe3d546ae4
victorkimathi <victorcodes9532@gmail.com>
2024-12-25 12:00:36 +0300

    Contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
+import Contact from "./pages/contact/page";
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    // <h1 className="text-3xl font-bold underline text-red-500">Hello world</h1>
+    <Contact />
+
   );
 }
 
