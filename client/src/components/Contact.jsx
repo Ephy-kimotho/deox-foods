17ea074dccf2dee6ef1e96c2b40921250ce9ca5f
Ericode254 <jilloerick6@gmail.com>
2024-12-30 13:09:10 +0300

    add submition functionality of the form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+
 const Contact = () => {
+  const [isSending, setIsSending] = useState(false)
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="min-h-screen bg-zinc-200 dark:bg-night-200 flex flex-col items-center py-10 flex-grow">
       <h1 className="text-4xl font-bold text-gray-700 dark:text-orange-600 mt-16 mb-6">
@@ -24,6 +40,8 @@ const Contact = () => {
               name="name"
               className="mt-1 py-2 pl-3 w-full rounded-md  border border-gray-800 focus:outline-2 focus:border-none focus:outline-orange-200 placeholder:text-gray-500"
               placeholder="John Doe"
+              value={formData.name}
+              onChange={handleChange}
               required
             />
           </div>
@@ -41,6 +59,8 @@ const Contact = () => {
               name="email"
               className="mt-1 py-2 pl-3 w-full rounded-md  border border-gray-800 focus:outline-2 focus:border-none focus:outline-orange-200 placeholder:text-gray-500"
               placeholder="youremail@example.com"
+              value={formData.email}
+              onChange={handleChange}
               required
             />
           </div>
@@ -59,14 +79,22 @@ const Contact = () => {
               className="mt-1 py-2 pl-3 w-full rounded-md  border border-gray-800 focus:outline-2 focus:border-none focus:outline-orange-200 placeholder:text-gray-500"
               required
               placeholder="Write your message here..."
+              value={formData.message}
+              onChange={handleChange}
             ></textarea>
           </div>
 
           <button
             type="submit"
             className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+            disabled={isSending}
           >
-            Send Message
+            {isSending ? (
+              <span className="flex items-center">
+                <LoadingSpinner />
+                Sending...
+              </span>
+            ) : "Send message"}
           </button>
         </form>
       </div>
