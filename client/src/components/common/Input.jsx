45d31d4bd66b0b2118e8c6dfe150b598eafebd91
Ephy Kimotho <ephyk48@gmail.com>
2025-01-06 18:31:56 +0300

    updated Input.jsx file to handle Icons dynamically

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,9 +1,29 @@
 /* eslint-disable react/prop-types */
 import { useField } from "formik";
 import { FaUser, FaEnvelope } from "react-icons/fa";
+import { FaKey, FaHouse } from "react-icons/fa6";
 
 function Input({ type, ...props }) {
   const [fields, meta] = useField(props);
+
+  let Icon = null;
+
+  if (props.name === "hostel" || props.name === "plotName") {
+    Icon = FaHouse;
+  }
+
+  if (props.name === "roomNo" || props.name === "houseNo") {
+    Icon = FaKey;
+  }
+
+  if (props.name === "email") {
+    Icon = FaEnvelope;
+  }
+
+  if (props.name === "username") {
+    Icon = FaUser;
+  }
+
   return (
     <section className="w-full">
       <div
@@ -21,7 +41,7 @@ function Input({ type, ...props }) {
           type="button"
           className="w-10 flex justify-center items-center cursor-default"
         >
-          {type === "text" ? <FaUser /> : <FaEnvelope />}
+          <Icon />
         </button>
       </div>
       {meta.touched && meta.error && (
