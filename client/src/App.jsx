eb92c87e5081585b5f4f9635518e1f1fe6b8f939
Ephy Kimotho <ephyk48@gmail.com>
2025-01-06 18:44:11 +0300

    updated main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import AuthProvider from "./components/AuthProvider";
 
 function App() {
   return (
-    <RouterProvider router={routes} future={{ v7_startTransition: true }} />
+    <AuthProvider>
+      <RouterProvider router={routes} future={{ v7_startTransition: true }} />
+    </AuthProvider>
   );
 }
 export default App;
