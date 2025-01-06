b4aea305bb255b553dc9058b759746f4ed28bf5e
Ephy Kimotho <ephyk48@gmail.com>
2025-01-06 18:40:06 +0300

    updated login & signup forms to disable submit button when submitting

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Formik, Form } from "formik";
+import { useState, useContext } from "react";
+import { authContext } from "./AuthProvider";
 import AuthButton from "./common/AuthButton";
 import Input from "./common/Input";
 import PasswordInput from "./common/PasswordInput";
 import * as Yup from "yup";
-import { useState } from "react";
-import LoadingSpinner from "./LoadingSpinner";
-import axios from "axios";
 
 const schema = Yup.object({
   username: Yup.string()
@@ -23,30 +22,20 @@ const schema = Yup.object({
 
 function Signup() {
   const [showPassword, setShowPassword] = useState(false);
-  const [showSpinner, setShowSpinner] = useState(false); // Local state for spinner visibility
+  const { signup } = useContext(authContext);
+  const navigate = useNavigate();
 
   const handleSubmit = async (values, actions) => {
-    // Set the form as submitting and show spinner
-    actions.setSubmitting(true);
-    setShowSpinner(true); // Show the spinner
+    console.log("Values: ", values);
 
-    // Introduce a timeout to simulate delay and ensure spinner is visible
-    const timeout = setTimeout(async () => {
-      try {
-        const response = await axios.post("/api/signup", values);
-        console.log("Response: ", response.data);
-
-        // Reset the form on success
-        actions.resetForm();
-      } catch (error) {
-        console.error("Error during form submission: ", error);
-      } finally {
-        // Clear the timeout and stop submitting
-        clearTimeout(timeout);
-        actions.setSubmitting(false);
-        setShowSpinner(false); // Hide the spinner after the delay
-      }
-    }, 500); // Adjust the timeout duration as needed (e.g., 500ms)
+    /* REPLACE PROMISE WITH SIGN UP LOGIC */
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(), 1500);
+    });
+    signup();
+    actions.resetForm();
+    actions.setSubmitting(false);
+    navigate("/");
   };
 
   const togglePassword = () => setShowPassword((state) => !state);
@@ -58,49 +47,38 @@ function Signup() {
         validationSchema={schema}
         onSubmit={handleSubmit}
       >
-        {({ isSubmitting, values, handleChange }) => (
-          <Form className="bg-gray-200 rounded-md w-11/12 max-w-2xl px-6 py-5 flex flex-col items-center gap-5">
-            <h2 className="text-xl sm:text-3xl text-night-200 font-bold uppercase">
-              Create your account
-            </h2>
-            <Input
-              type="text"
-              name="username"
-              placeholder="Enter your username."
-              value={values.username}
-              onChange={handleChange}
-            />
-            <Input
-              type="email"
-              name="email"
-              placeholder="Enter your email."
-              value={values.email}
-              onChange={handleChange}
-            />
-            <PasswordInput
-              type={showPassword ? "text" : "password"}
-              name="password"
-              placeholder="Enter your password."
-              handleToggle={togglePassword}
-              showPassword={showPassword}
-              value={values.password}
-              onChange={handleChange}
-            />
-            <AuthButton disabled={isSubmitting}>
-              {showSpinner ? <LoadingSpinner /> : "Create Account"}
-            </AuthButton>
-            <div className="font-bold font-openSans text-base text-night-200 flex flex-col items-center sm:flex-row gap-2">
-              <p>Already have an account ?</p>
-              <Link to="/login" className="text-orange-200 hover:underline">
-                Log in
-              </Link>
-            </div>
-          </Form>
-        )}
+        <Form className="bg-gray-200 rounded-md w-11/12 max-w-2xl px-6 py-5 flex flex-col items-center gap-5">
+          <h2 className="text-xl sm:text-3xl text-night-200 font-bold uppercase">
+            Create your account
+          </h2>
+          <Input
+            type="text"
+            name="username"
+            placeholder="Enter your username."
+          />
+
+          <Input type="email" name="email" placeholder="Enter your email." />
+
+          <PasswordInput
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Enter your password."
+            handleToggle={togglePassword}
+            showPassword={showPassword}
+          />
+
+          <AuthButton action="creating account...">Create account</AuthButton>
+
+          <div className="font-bold font-openSans text-base text-night-200 flex flex-col items-center sm:flex-row gap-2">
+            <p>Already have an account ?</p>
+            <Link to="/login" className="text-orange-200 hover:underline">
+              Log in
+            </Link>
+          </div>
+        </Form>
       </Formik>
     </section>
   );
 }
 
 export default Signup;
-
